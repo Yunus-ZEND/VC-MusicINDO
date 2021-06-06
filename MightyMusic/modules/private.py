d501cf18681f899eb89c44033c065339b0f1914c
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-06-06 08:24:52 +0700

    Update private.py

--- MightyMusic/modules/private.py
+++ MightyMusic/modules/private.py
@@ -21,7 +21,7 @@ def _start(client, message):
                     InlineKeyboardButton(
                         "🏷 Group", url=f"https://t.me/{SUPPORT_GROUP}"),
                     InlineKeyboardButton(
-                        "🦠 Help",url="https://t.me/{}?start=help".format(bot.username))
+                        "🦠 Help",url="https://t.me/{}?start=help".format({BOT_USERNAME}))
                 ]
             ]
         ),
