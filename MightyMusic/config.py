284cdd075ce6e3c044cd3e4dcb00a9c4a391770a
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-06-06 20:19:58 +0700

    Update config.py

--- MightyMusic/config.py
+++ MightyMusic/config.py
@@ -24,5 +24,6 @@ SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Yunus-ZEND/VC-MusicINDO")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 PMPERMIT = getenv("PMPERMIT", None)
+OWNER_NAME = getenv ("OWNER_NAME", "ZendYNS")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
