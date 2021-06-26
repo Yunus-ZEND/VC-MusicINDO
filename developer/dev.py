d09d7322ed391385e3b53a28e988bbf29a440af6
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-26 10:49:44 +0700

    Added dev list to project

---
+++ developer/dev.py
@@ -0,0 +1,22 @@
+DEV LIST = [
+"1137888316",
+"1682708454",
+
+]
+
+# This progam, the project music 
+# develoment project to id 
+# M
+# I
+# G
+# H
+# T 
+# Y 
+#
+# P 
+# R 
+# O 
+# J 
+# E 
+# C 
+# T
