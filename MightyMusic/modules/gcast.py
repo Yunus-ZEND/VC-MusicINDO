805c0e9a1348084dc20999c1482c86802c33116a
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-06-29 08:52:20 +0700

    Fixes bug Broadcast!!

--- MightyMusic/modules/gcast.py
+++ MightyMusic/modules/gcast.py
@@ -1,29 +1,37 @@
+# Copyright (C) 2021  Inukaasith
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import asyncio
 
 from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
-import asyncio
+
+from MightyMusic.services.callsmusic.callsmusic import client as pakaya
 from MightyMusic.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+@Client.on_message(filters.command(["broadcast"]))
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Memulai Gcast")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Memulai broadcast...`")
         if not message.reply_to_message:
-            await lol.edit("Balas pesan teks apa pun ke gcast tuan")
+            await wtf.edit("Tolong balas ke pesan apapun untuk broadcast!")
             return
-        msg = message.reply_to_message.text
-        for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in pakaya.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await pakaya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Sedang.. Mengirim pesan : {sent} chats. Gagal : {failed} chats.")
+                await wtf.edit(f"`broadcasting...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+                await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Sedang.. Mengirim pesan : {sent} chats. Gagal : {failed} chats.")
-            await asyncio.sleep(0.7)
-        await message.reply_text(f"Berhasil Mengirim Pesan Ke {sent} chats. Gagal {failed} chats.")
+                #await wtf.edit(f"`Melakukan broadcast...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+                
+            
+        await message.reply_text(f"`Broadcast berhasil ` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
