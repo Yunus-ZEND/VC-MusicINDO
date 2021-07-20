1072fe7bf716ab06009ec1393b5536b92be10a94
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-07-21 06:26:43 +0700

    Update gcast.py

--- MightyMusic/modules/gcast.py
+++ MightyMusic/modules/gcast.py
@@ -11,14 +11,14 @@ from pyrogram.errors import UserAlreadyParticipant
 from MightyMusic.services.callsmusic.callsmusic import client as pakaya
 from MightyMusic.config import SUDO_USERS
 
-@Client.on_message(filters.command(["broadcast"]))
+@Client.on_message(filters.command(["gcast"]))
 async def broadcast(_, message: Message):
     sent=0
     failed=0
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Memulai broadcast...`")
+        wtf = await message.reply("`Memulai global cast...`")
         if not message.reply_to_message:
             await wtf.edit("Tolong balas ke pesan apapun untuk broadcast!")
             return
@@ -27,11 +27,11 @@ async def broadcast(_, message: Message):
             try:
                 await pakaya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`broadcasting...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+                await wtf.edit(f"`global cast...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                #await wtf.edit(f"`Melakukan broadcast...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+                #await wtf.edit(f"`Melakukan gcast...` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
                 
             
-        await message.reply_text(f"`Broadcast berhasil ` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
+        await message.reply_text(f"`gcast berhasil ` \n\n**Mengirim ke:** `{sent}` Chats \n**gagal di:** {failed} Chats")
