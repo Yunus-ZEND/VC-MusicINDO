ff397c44a7562dea169698e0f7d75c295b70d8f3
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-07-21 06:51:31 +0700

    Update __init__.py

--- MightyMusic/services/callsmusic/queues/__init__.py
+++ MightyMusic/services/callsmusic/queues/__init__.py
@@ -1,3 +1,3 @@
-from MightyMusic.services.queues.queues import clear, get, is_empty, put, task_done
+from MightyMusic.services.callsmusic.queues.queues import clear, get, is_empty, put, task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
