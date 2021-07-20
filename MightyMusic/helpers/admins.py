ce69f7ebe46ccc2b5d77df869e2d78a65df5cf68
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-07-21 06:17:41 +0700

    Update admins.py

--- MightyMusic/helpers/admins.py
+++ MightyMusic/helpers/admins.py
@@ -2,8 +2,8 @@ from typing import List
 
 from pyrogram.types import Chat
 
-from DaisyXMusic.function.admins import get as gett
-from DaisyXMusic.function.admins import set
+from MightyMusic.function.admins import get as gett
+from MightyMusic.function.admins import set
 
 
 async def get_administrators(chat: Chat) -> List[int]:
