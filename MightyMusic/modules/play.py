08af0b9f3780a9fbd7f5c9a0be57cdf3ec73f79f
farizjr1 <87758609+farizjr1@users.noreply.github.com>
2021-07-24 02:28:50 +0700

    Update play.py

--- MightyMusic/modules/play.py
+++ MightyMusic/modules/play.py
@@ -509,7 +509,7 @@ async def play(_, message: Message):
     elif urls:
         query = toxt
         await lel.edit("🎵 **Sedang Memproses**")
-        ydl_opts = {"format": "bestaudio/best"}
+        ydl_opts = {"format": "141/bestaudio[ext=m4a]"}
         try:
             results = YoutubeSearch(query, max_results=1).to_dict()
             url = f"https://youtube.com{results[0]['url_suffix']}"
